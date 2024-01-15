4887e46e5cb8e8e040eac2ec9744366aa923fe67
Masoud bahamin <Bahaminwp@gmail.com>
2024-01-15 17:21:12 +0300

    handel save image with blob

--- app/(pages)/account/page.tsx
+++ app/(pages)/account/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
+import type { PutBlobResult } from '@vercel/blob';
 
 
 
@@ -8,6 +9,11 @@ function Account() {
 
     const [image, setImage] = useState<null | File>(null)
     const [userInfo, setUserInfo] = useState<{ email: string, _id: string, image: any }>()
+
+    const inputFileRef = useRef<HTMLInputElement>(null);
+    const [blob, setBlob] = useState<PutBlobResult | null>(null);
+
+    console.log(blob);
 
     const getMe = async () => {
         const res = await fetch("http://localhost:3000/api/auth/me")
@@ -21,40 +27,78 @@ function Account() {
         getMe()
     }, [])
 
-    const saveImage = async () => {
-        if (!image || !userInfo) {
-            return false
-        }
+    // const saveImage = async () => {
+    //     if (!image || !userInfo) {
+    //         return false
+    //     }
 
-        const formData = new FormData()
-        formData.append("image", image);
-        formData.append("userId", userInfo._id);
+    //     const formData = new FormData()
+    //     formData.append("image", image);
+    //     formData.append("userId", userInfo._id);
 
-        const res = await fetch("http://localhost:3000/api/img/create", {
-            method: "POST",
-            body: formData
-        })
-        if (res.ok) {
-            getMe()
-        }
-        console.log(res);
+    //     const res = await fetch("http://localhost:3000/api/img/create", {
+    //         method: "POST",
+    //         body: formData
+    //     })
+    //     if (res.ok) {
+    //         getMe()
+    //     }
+    //     console.log(res);
 
 
-    }
+    // }
 
     return (
         <div className="p-10 bg-green-100">Account page <p className="p-4 bg-rose-100"> {userInfo?.email || "plaese login"}</p>
             <div className="m-10">
                 <img className="w-40 h-40"
-                    src={`/uploads/${userInfo?.image[userInfo.image.length - 1]?.name}`} alt="" />
+                    src={`${userInfo?.image[userInfo.image.length - 1]?.name}`} alt="" />
             </div>
-            <div className="m-10">
+            {/* <div className="m-10">
                 <input type="file" onChange={e => {
                     const file = e.target?.files && e.target.files[0]
                     setImage(file || null)
                 }} />
                 <button className="bg-green-200 p-2" onClick={saveImage}>save</button>
-            </div>
+            </div> */}
+            <>
+                <h1>Upload Your Avatar</h1>
+
+                <form
+                    onSubmit={async (event) => {
+                        event.preventDefault();
+
+                        if (!inputFileRef.current?.files) {
+                            throw new Error("No file selected");
+                        }
+
+                        const file = inputFileRef.current.files[0];
+
+                        const response = await fetch(
+                            `/api/file/${file.name}/${userInfo?._id}`,
+                            {
+                                method: 'POST',
+                                body: file,
+                            },
+                        );
+
+                        const newBlob = (await response.json()) as PutBlobResult;
+
+                        setBlob(newBlob);
+                        getMe()
+                    }}
+                >
+                    <input name="file" ref={inputFileRef} type="file" required />
+                    <button type="submit">Upload</button>
+                </form>
+                {blob && (
+                    <div>
+                        Blob url: <a href={blob.url}>{blob.url}</a>
+                        <br />
+                        <img src={blob.url} alt="" />
+                    </div>
+                )}
+            </>
         </div>
     )
 }
---
+++ app/api/file/[...file]/route.ts
@@ -0,0 +1,37 @@
+import { imageModel } from '@/models/imageModel';
+import { connecctToDB } from '@/utils/coneccetToDB';
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+import { Context } from 'vm';
+
+export async function POST(request: Request , context : Context): Promise<NextResponse> {
+    const params :  string[]  = context.params.file 
+    console.log( "oooooooo" ,params);
+    
+
+    if (request.body ) {
+
+        const blob = await put(params[0], request.body, {
+            access: 'public',
+        });
+
+        connecctToDB()
+        
+        const userId = "65a28cad5bb94409be665a17"
+
+        const createImage = await imageModel.create({ name: blob.url,userId : params[1] })
+
+
+        return NextResponse.json(blob);
+
+    } else {
+        return NextResponse.json({ message: "file not found" });
+    }
+}
+
+// The next lines are required for Pages API Routes only
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
